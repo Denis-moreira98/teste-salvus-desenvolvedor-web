7498b4c196806b56db1cdd045986eaf5ab9864d3
Denis Moreira <silvad281@gmail.com>
2024-07-11 22:02:51 -0300

    feat: buscando produtos por id, criação da classe GetProductByIdController

---
+++ backend/src/controllers/GetProductByIdController.ts
@@ -0,0 +1,17 @@
+import { Request, Response } from "express";
+import GetProductByIdService from "../services/GetProductByIdService";
+
+class GetProductByIdController {
+   public async handle(req: Request, res: Response) {
+      const { id } = req.params;
+      try {
+         const getProductById = new GetProductByIdService();
+         const product = await getProductById.execute(id);
+         res.status(200).json(product);
+      } catch (error) {
+         res.status(404).json({ error: error.message });
+      }
+   }
+}
+
+export default GetProductByIdController;
