679158d6ebfabc5d09b720d181d297eaff2b0bac
Denis Moreira <silvad281@gmail.com>
2024-07-11 22:01:08 -0300

    feat: buscando produtos por id, criação da classe GetProductByIdService

---
+++ backend/src/services/GetProductByIdService.ts
@@ -0,0 +1,13 @@
+import Product from "../models/productModel";
+
+class GetProductByIdService {
+   public async execute(productId: string) {
+      const product = await Product.findByPk(productId);
+      if (!product) {
+         throw new Error("Product not found");
+      }
+      return product;
+   }
+}
+
+export default GetProductByIdService;
