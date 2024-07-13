7a0ab7dd1a6f52f8ae3a9dbe90ad60c8f4c0a41f
Denis Moreira <silvad281@gmail.com>
2024-07-13 09:25:39 -0300

    feat: add toast de mensagem

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -7,10 +7,14 @@ import { router } from "./App.tsx";
 import { RouterProvider } from "react-router-dom";
 import ProductProvider from "./contexts/ProductContext.tsx";
 
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
    <React.StrictMode>
       <ProductProvider>
          <RouterProvider router={router} />
+         <ToastContainer />
       </ProductProvider>
    </React.StrictMode>
 );
